44b1ce819e2573d2b1aca1db604af6e33f81d27e
Aolin <aolin.zhang@pingcap.com>
2022-07-10 22:09:07 +0800

    use commander to parse command-line args

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const { Command } = require("commander");
+
+const deref = require("./deref.js");
+
+const program = new Command();
+program
+    .name("postprocess")
+    .description("Postprocess a OpenAPI document for ReDoc")
+    .version("0.1.0");
+program.command("deref")
+    .description("Use $RefParser to dereference a JSON schema")
+    .argument("<in-filename>", "Input JSON file")
+    .argument("<out-filename>", "Output JSON file")
+    .action(deref);
+
+program.parse();
